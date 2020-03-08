b092bd6dcbf628a91eb7151266e72588654d2764
Ivan Sebastian <ivansedbert@gmail.com>
2020-03-08 15:54:16 +0700

    initial commit

---
+++ nanochallengeMartinus/LogoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LogoViewController.swift
+//  nanochallengeMartinus
+//
+//  Created by Ivan Sebastian on 08/03/20.
+//  Copyright © 2020 Vanski Corp. All rights reserved.
+//
+
+import UIKit
+
+class LogoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
