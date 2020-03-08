782e37ae61746d074385fdba7a82d5ffce75d9d1
Ivan Sebastian <ivansedbert@gmail.com>
2020-03-08 15:50:53 +0700

    Initial Commit

---
+++ nanochallengeMartinus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nanochallengeMartinus
+//
+//  Created by Ivan Sebastian on 08/03/20.
+//  Copyright © 2020 Vanski Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
